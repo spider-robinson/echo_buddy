f2e05f4745b4484d2b0ade3118eca1e3633dc4bd
Jaden Tennis <jade.entling@gmail.com>
2017-08-04 00:42:16 -0400

    Lilian: fixed WordBuddy

--- word_buddy/analogy_buddy.py
+++ word_buddy/analogy_buddy.py
@@ -11,10 +11,8 @@ from gensim.models.keyedvectors import KeyedVectors
 app = Flask(__name__)
 ask = Ask(app, '/')
 
-global glove
+glove = None
 path = "glove.6B.50d.txt.w2v"
-glove = KeyedVectors.load_word2vec_format(path, binary=False)
-
 
 @app.route('/')
 def homepage():
@@ -22,6 +20,7 @@ def homepage():
 
 @ask.launch
 def start_skill():
+    print("Skill started")
     msg = "Hi, I'm Word Buddy. What question do you have for me?"
     return question(msg)
 
@@ -48,6 +47,8 @@ def analogythis(thatone, thistwo, thattwo, k=3):
     analogythis(man, queen, woman, k=1)
         returns ["king"]
     """
+    if thatone is None or thistwo is None or thattwo is None:
+        return False
     query = glove.wv[thistwo] - glove.wv[thattwo] + glove.wv[thatone]
     thisone = np.array(glove.wv.similar_by_vector(query))[:k,0].tolist()
     print(thatone)
@@ -55,7 +56,10 @@ def analogythis(thatone, thistwo, thattwo, k=3):
 
 @ask.intent("ThisIntent")
 def solvethis(thatone, thistwo, thattwo):
-    thisone = ", or ".join(analogythis(thatone, thistwo, thattwo))
+    result = analogythis(thatone, thistwo, thattwo)
+    if result is False:
+        return question("I didn't get that. Try again, or try another analogy.")
+    thisone = ", or ".join(result)
     answer_msg = "{}, is to {}, as {}, is to, {}".format(thisone, thatone, thistwo, thattwo)
     return statement(answer_msg)
 
@@ -82,6 +86,8 @@ def analogythat(thisone, thistwo, thattwo, k=3):
     analogythis(king, queen, woman, k=1)
         returns ["man"]
     """
+    if thisone is None or thistwo is None or thattwo is None:
+        return False
     query = glove.wv[thisone] + glove.wv[thattwo] - glove.wv[thistwo]
     thatone = np.array(glove.wv.similar_by_vector(query))[:k,0].tolist()
     print(thatone)
@@ -89,7 +95,10 @@ def analogythat(thisone, thistwo, thattwo, k=3):
 
 @ask.intent("ThatIntent")
 def solvethis(thisone, thistwo, thattwo):
-    thatone = ", or ".join(analogythat(thisone, thistwo, thattwo))
+    result = analogythat(thisone, thistwo, thattwo)
+    if result is False:
+        return question("I didn't get that. Try again, or try a different analogy.")
+    thatone = ", or ".join(result)
     answer_msg = "{}, is to {}, as {}, is to, {}".format(thisone, thatone, thistwo, thattwo)
     return statement(answer_msg)
 
@@ -100,4 +109,6 @@ def tell_related(word):
     return statement(msg)
 
 if __name__ == '__main__':
+    glove = KeyedVectors.load_word2vec_format(path, binary=False)
+    print("glove loaded")
     app.run(port=5010, debug=True)
